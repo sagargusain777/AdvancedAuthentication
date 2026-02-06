c86f2818217ba52134ae77c1c15ff77267e61268
Sagar Singh Gusain <sagargusain201012@gmail.com>
2026-02-07 03:16:02 +0530

    Initial Backend Setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World!")
+})
+app.listen(3000,()=>{
+    console.log("Server is running in port 3000")
+})
+
+
+
